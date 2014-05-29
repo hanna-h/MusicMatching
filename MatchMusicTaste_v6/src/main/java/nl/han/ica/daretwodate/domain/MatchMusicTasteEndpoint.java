52757a7ebb01ac73161f2bd4f2aab425fd8cf64d
Jansuun <jpjjansen92@hotmail.com>
2014-05-29 13:20:39 +0200

    Added geolocation service

---
+++ MatchMusicTaste_v6/src/main/java/nl/han/ica/daretwodate/domain/MatchMusicTasteEndpoint.java
@@ -0,0 +1,61 @@
+package nl.han.ica.daretwodate.domain;
+
+import nl.han.ica.daretwodate.*;
+import nl.han.ica.daretwodate.dao.iMatchMusicTasteDao;
+import nl.han.ica.daretwodate.dao.MatchMusicTasteDao;
+import nl.han.ica.daretwodate.model.Users;
+import nl.han.ica.daretwodate.services.*;
+
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+
+import java.util.List;
+
+@Endpoint
+public class MatchMusicTasteEndpoint {
+    private final iMatchMusicTasteDao matchMusicTasteDao;
+    private Marshaller marshaller;
+    private Unmarshaller unmarshaller;
+    private ICurrentLocation currentLocation;
+    private EventsForArtist eventsForArtist;
+    private JoinEvent joinEvent;
+    private LoginOnLastfm loginOnLastfm;
+    private ITasteOMeter tasteOMeter;
+
+    public MatchMusicTasteEndpoint(Marshaller marshaller, Unmarshaller unmarshaller, CurrentLocation currentLocation,
+                                   EventsForArtist eventsForArtist, JoinEvent joinEvent, LoginOnLastfm loginOnLastfm,
+                                   ITasteOMeter tasteOMeter, iMatchMusicTasteDao matchMusicTasteDao) {
+        this.marshaller = marshaller;
+        this.unmarshaller = unmarshaller;
+        this.currentLocation = currentLocation;
+        this.eventsForArtist = eventsForArtist;
+        this.joinEvent = joinEvent;
+        this.loginOnLastfm = loginOnLastfm;
+        this.tasteOMeter = tasteOMeter;
+        this.matchMusicTasteDao = matchMusicTasteDao;
+
+        matchMusicTasteDao.getUsers();
+    }
+
+    @SuppressWarnings({"unchecked", "deprecation"})
+    @PayloadRoot(localPart = "MatchMusicTasteRequest", namespace = "http://www.han.nl/schemas/messages")
+    public MatchMusicTasteResponse doMusicMatching(MatchMusicTasteRequest req) {
+        System.out.println(matchMusicTasteDao.getUser("fairyglen").getLastfmUsername());
+
+        String lastfmApiKey = req.getInput().getLastfmApiKey();
+
+        MatchMusicTasteResult result = new MatchMusicTasteResult();
+        result.setPercentage(56);
+        MatchMusicTasteResponse resp = new MatchMusicTasteResponse();
+        resp.setResult(result);
+
+        tasteOMeter.callTasteOMeter("Jansuun","fairyglen","3");
+        System.out.println(currentLocation.getCountry("89.98.201.249"));
+
+
+        return resp;
+    }
+
+}
---
+++ MatchMusicTaste_v6/src/main/java/nl/han/ica/daretwodate/services/CurrentLocation.java
@@ -0,0 +1,55 @@
+package nl.han.ica.daretwodate.services;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
+public class CurrentLocation implements ICurrentLocation {
+    public String getCountry(String ipaddress)
+    {
+        String result = new String();
+
+         try {
+
+                String uri = "http://freegeoip.net/xml/" + ipaddress;
+
+                URL url = new URL(uri);
+                HttpURLConnection connection =
+                (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Accept", "application/xml");
+
+                InputStream xml = connection.getInputStream();
+
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(xml);
+
+                NodeList country = doc.getElementsByTagName("CountryName");
+                Element line = (Element) country.item(0);
+                System.out.println("country: " + getCharacterDataFromElement(line));
+
+                result = getCharacterDataFromElement(line);
+
+         }
+            catch(Exception e)
+            {
+                System.out.println("Exception: " + e.getMessage());
+            }
+
+        return result;
+    }
+
+    public static String getCharacterDataFromElement(Element e)
+    {
+        Node child = e.getFirstChild();
+        if (child instanceof CharacterData)
+        {
+            CharacterData cd = (CharacterData) child;
+            return cd.getData();
+        }
+        return "?";
+    }
+}
