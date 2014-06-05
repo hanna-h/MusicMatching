48011e3add0e9e6e6f8ea69378d194553d159cdd
Hanna Hermsen <hannahermsen@gmail.com>
2014-06-05 13:22:04 +0200

    Added JMS_v1

---
+++ JMS_v1/src/main/java/creditcard/check/jms/ExternalValidateCreditcardReplier.java
@@ -0,0 +1,50 @@
+package creditcard.check.jms;
+
+/**
+ * Created by root on 5-6-14.
+ */
+
+import nl.han.dare2date.*;
+import nl.han.dare2date.service.jms.util.Replier;
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.ServerSession;
+import javax.jms.JMSException;
+import javax.jms.ObjectMessage;
+import javax.naming.NamingException;
+import java.io.Serializable;
+
+public class ExternalValidateCreditcardReplier extends Replier {
+    private Creditcard card;
+
+    protected ExternalValidateCreditcardReplier(Connection connection,
+        String requestQueueName, String invalidQueueName)
+    throws JMSException, NamingException {
+        super(connection, requestQueueName, invalidQueueName);
+    }
+
+    @Override
+    public ObjectMessage getReplyMessage() {
+        ObjectMessage msg = null;
+        try {
+            msg = getSession().createObjectMessage();
+            boolean isValid = false;
+            if (card.getCvc() == 1234 && card.getNumber() == 4321) {
+                isValid = true;
+            }
+            ValidateCreditcardResponse response =
+                    new ValidateCreditcardResponse(isValid);
+            msg.setObject(response);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return msg;
+    }
+
+    @Override
+    public void handleMessage(Serializable contents) {
+        ValidateCreditcardRequest request =
+                (ValidateCreditcardRequest) contents;
+        this.card = request.getCard();
+    }
+}
---
+++ JMS_v1/src/main/java/nl/han/dare2date/service/jms/util/Queues.java
@@ -0,0 +1,11 @@
+package nl.han.dare2date.service.jms.util;
+
+/**
+ * Created by root on 5-6-14.
+ */
+public interface Queues {
+    String REQUEST_QUEUE = "RequestQueue";
+    String RESPONSE_QUEUE = "ResponseQueue";
+    String INVALID_QUEUE = "InvalidQueue";
+    String UPDATE_TOPIC_NAME = "UpdateTopicName";
+}
